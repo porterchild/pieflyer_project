1e0ef77c24ae5dcd2d05348c596639f99f9f43a1
Porter Child <pchild28@gmail.com>
2017-04-21 18:20:31 +0000

    used TemplateView to render the whole game in one html file

--- pieflyer/urls.py
+++ pieflyer/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.views.generic.base import TemplateView
 from . import views
 
 app_name = 'pieflyer'
 urlpatterns = [
-    url(r'^$', views.game, name='index'),
+    ##url(r'^$', views.game, name='game'),
+    url(r'^$', TemplateView.as_view(template_name='templates/gamedoc.html')),
 ]
--- pieflyer/views.py
+++ pieflyer/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.template import Context, loader
 
 # Create your views here.
 def game(request):
-    return render(request, 'pieflyer/gamedoc')
+    template = loader.get_template("pieflyer/gamedoc.html")
+    return HttpResponse(template.render)
+
+    
+
+
+def index(request):
+    template = loader.get_template("app/index.html")
+    return HttpResponse(template.render)
