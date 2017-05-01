b1d7d58807af7b2ac97ddc0e2d248ef730fb4a71
Porter Child <pchild28@gmail.com>
2017-05-01 05:55:34 +0000

    still stuck on routing issue. Updated css and other small stuff.

--- pieflyer/urls.py
+++ pieflyer/urls.py
@@ -6,5 +6,6 @@ app_name = 'pieflyer'
 urlpatterns = [
     url(r'^', views.index, name='index'),
     url(r'^record_score/$', views.record_score, name='record_score'),
+    ##url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     ##url(r'^$', TemplateView.as_view(template_name='pieflyer/gamedoc.html')),
 ]
